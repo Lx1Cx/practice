416448b5be524cfbdd484c3ee6a2c62742351c21
lx1cx <lagunov.2019@list.ru>
2024-01-17 16:38:25 +0500

    Написан метод для авторизации

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { LoginDTO } from './dto/LoginDTO'
 import { PrismaService } from '../database/Database'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class UsersService {
 
-    constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService, private jwt: JwtService) {}
 
     async loginAsync(requestData: LoginDTO) {
 
         const {login, password} = requestData
 
-        const user = this.prisma.user.findUnique({
+        const user = await this.prisma.user.findUnique({
             where: {
-                login:
+                login: login,
+                password: password
             }
         })
+
+        if (!user) {
+            throw new UnauthorizedException({
+                displayMessage: "Неверный логин или пароль"
+            })
+        }
+
+        return {
+            accessToken: await this.jwt.signAsync({
+                id: user.id,
+                login: user.login,
+                role: user.role
+            }, {secret: "secret", expiresIn: "1d"}),
+        }
+   }
+
+   async registrationAsync() {
+
    }
 }
