173a937c7a6f0d382b84fc914a0fdf3784781b4e
lx1cx <lagunov.2019@list.ru>
2024-01-17 16:36:45 +0500

    Написан LoginDTO

--- src/users/dto/LoginDTO.ts
+++ src/users/dto/LoginDTO.ts
@@ -1,11 +1,18 @@
-import { IsString, Length, minLength } from 'class-validator'
+import { IsString, Length } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class LoginDTO {
 
+    @ApiProperty({
+        default: "tester"
+    })
     @IsString()
     @Length(6, 12)
     login: string
 
+    @ApiProperty({
+        default: "tester"
+    })
     @IsString()
     @Length(6, 12)
     password: string
