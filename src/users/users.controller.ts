2985bad1287f014ac97d0076d45f4b4b01fff98e
lx1cx <lagunov.2019@list.ru>
2024-01-17 16:37:29 +0500

    Документация для сваггера

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,34 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, HttpCode, Post } from '@nestjs/common'
 import { UsersService } from './users.service';
 import { LoginDTO } from './dto/LoginDTO'
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags, ApiUnauthorizedResponse,
+  getSchemaPath,
+} from '@nestjs/swagger'
+import { UserDTO } from './dto/UserDTO'
+import { UnauthorizedException } from '../errors/UnauthorizedException'
 
+@ApiTags("users")
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @HttpCode(200)
+  @ApiOperation({summary: "Авторизация пользователя"})
+  @ApiOkResponse({
+    status: 200,
+    type: UserDTO
+  })
+  @ApiUnauthorizedResponse({
+    type: UnauthorizedException
+  })
   @Post("/login")
-  async login(@Body() loginDTO: LoginDTO) {
-    return await this.usersService
+  async login(@Body() loginDTO: LoginDTO): Promise<UserDTO> {
+    return await this.usersService.loginAsync(loginDTO)
   }
 }
