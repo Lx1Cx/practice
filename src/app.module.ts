515fa33301af11bcdb9391a5c37ad6d7c507f8db
lx1cx <lagunov.2019@list.ru>
2024-01-17 16:37:07 +0500

    Логирование

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module';
+import { AppLoggerMiddleware } from './middleware/AppLoggerMiddleware'
 
 @Module({
-    imports: [UsersModule, UsersModule],
+    imports: [UsersModule],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer): void {
+        consumer.apply(AppLoggerMiddleware).forRoutes('*');
+    }
+}
