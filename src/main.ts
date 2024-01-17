0e825d8a6fa633bc4b3e5d12824595253ec8487b
lx1cx <lagunov.2019@list.ru>
2024-01-17 16:35:49 +0500

    Добавлена сваггер документация

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
-    await app.listen(3000)
+
+    const config = new DocumentBuilder()
+        .setTitle("Tour agency")
+        .build()
+
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup("/api", app, document)
+
+    await app.listen(5000)
+
+    console.log("Swagger working on http://localhost:5000/api")
 }
 bootstrap()
