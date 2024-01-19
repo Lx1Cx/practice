4179c397ba4826a4a9bc001f6e8c3674e275da19
lx1cx <lagunov.2019@list.ru>
2024-01-19 13:27:42 +0500

    Написаны контроллеры для получения туров, тура по id, удаления тура по id

--- src/tours/tours.controller.ts
+++ src/tours/tours.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Post, Put, Query } from '@nestjs/common'
 import { ToursService } from './tours.service';
-import { ApiBadRequestResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger'
+import { ApiBadRequestResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger'
 import { ICreateTourDTO } from './dto/ICreateTourDTO'
 import { BadRequestException } from '../errors/BadRequestException'
+import { TourEntity } from './TourEntity'
+import { NotFoundException } from '../errors/NotFoundException'
 
 @ApiTags("tours")
 @Controller('tours')
@@ -16,6 +18,41 @@ export class ToursController {
     type: BadRequestException
   })
   async create(@Body() requestData: ICreateTourDTO) {
-    return await this.toursService.create(requestData)
+    return await this.toursService.createAsync(requestData)
+  }
+
+  @Get("")
+  @ApiOperation({summary: "Получение списка тура"})
+  @ApiOkResponse({
+    type: TourEntity,
+    isArray: true
+  })
+  async getAll(): Promise<TourEntity[]> {
+    return await this.toursService.getAllAsync()
+  }
+
+  @Get(":tourId")
+  @ApiOperation({summary: "Получение тура по id"})
+  @ApiOkResponse({
+    type: TourEntity
+  })
+  @ApiNotFoundResponse({
+    type: NotFoundException
+  })
+  async getById(@Query("tourId") tourId: string): Promise<TourEntity> {
+    return await this.toursService.getByIdAsync(tourId)
+  }
+
+  @Delete(":tourId")
+  @ApiOperation({summary: "Удаление тура по id"})
+  @ApiOkResponse()
+  @ApiBadRequestResponse({
+    type: BadRequestException
+  })
+  @ApiNotFoundResponse({
+    type: NotFoundException
+  })
+  async deleteById(@Query("tourId") tourId: string) {
+    return await this.toursService.deleteByIdAsync(tourId)
   }
 }
