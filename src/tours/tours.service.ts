adec4a3b7511187527debc62acb242290b3d6d29
lx1cx <lagunov.2019@list.ru>
2024-01-19 13:28:05 +0500

    Написаны методы для получения туров, тура по id, удаления тура по id

--- src/tours/tours.service.ts
+++ src/tours/tours.service.ts
@@ -1,12 +1,13 @@
-import { BadGatewayException, Injectable } from '@nestjs/common'
+import { BadGatewayException, Injectable, NotFoundException } from '@nestjs/common'
 import { PrismaService } from '../database/Database'
 import { ICreateTourDTO } from './dto/ICreateTourDTO'
+import { TourEntity } from './TourEntity'
 
 @Injectable()
 export class ToursService {
     constructor(private readonly prisma: PrismaService) {}
 
-    async create({name, description, dateStart, dateEnd}: ICreateTourDTO) {
+    async createAsync({name, description, dateStart, dateEnd}: ICreateTourDTO) {
         const tour = await this.prisma.tour.findUnique({
             where: {
                 name
@@ -29,7 +30,35 @@ export class ToursService {
         })
     }
 
-    async getAll() {
+    async getAllAsync(): Promise<TourEntity[]> {
         return this.prisma.tour.findMany()
+    }
+
+    async getByIdAsync(tourId: string): Promise<TourEntity> {
+        return this.prisma.tour.findUnique({
+            where: {
+                id: tourId
+            }
+        })
+    }
+
+    async deleteByIdAsync(tourId: string) {
+        const tour = await this.prisma.tour.findUnique({
+            where: {
+                id: tourId
+            }
+        })
+
+        if (!tour) {
+            throw new NotFoundException({
+                displayMessage: "Тур с указанным id не найден"
+            })
+        }
+
+        return this.prisma.tour.delete({
+            where: {
+                id: tourId
+            }
+        })
     }
 }
