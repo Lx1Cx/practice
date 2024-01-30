ae0dfbc0df0ac37b8221fa1a56f9889abf0f1a72
lx1cx <lagunov.2019@list.ru>
2024-01-30 18:43:33 +0500

    Добавлены модули

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { UsersModule } from './users/users.module';
 import { AppLoggerMiddleware } from './middleware/AppLoggerMiddleware'
 import { OrdersModule } from './orders/orders.module';
 import { TourPlacesModule } from './tour-places/tour-places.module';
+import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static'
+import * as path from 'path'
 
 @Module({
-    imports: [UsersModule, OrdersModule, TourPlacesModule],
+    imports: [
+        UsersModule,
+        OrdersModule,
+        TourPlacesModule,
+        FilesModule,
+        ServeStaticModule.forRoot({
+            rootPath: path.resolve(__dirname, "..", "static")
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
