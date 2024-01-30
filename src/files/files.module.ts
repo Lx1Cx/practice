e8b94269668782aae0382ee54a794a8c9a1b11f1
lx1cx <lagunov.2019@list.ru>
2024-01-30 18:42:50 +0500

    Написан модуль для файлов

---
+++ src/files/files.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FilesController } from './files.controller';
+import { PrismaService } from '../database/Database'
+
+@Module({
+  controllers: [FilesController],
+  providers: [FilesService, PrismaService],
+})
+export class FilesModule {}
