3b0a5bffadbd3df8b6ca57964e8557b4ea8f7e53
lx1cx <lagunov.2019@list.ru>
2024-02-06 15:49:17 +0500

    Написана CreateTourDto

---
+++ src/tours/dto/CreateTourDto.ts
@@ -0,0 +1,40 @@
+import { IsArray, IsDate, IsInt, IsString } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
+
+export class CreateTourDto {
+    @ApiProperty()
+    @IsString()
+    name: string
+
+    @IsString()
+    @ApiProperty()
+    description: string
+
+    @IsInt()
+    @ApiProperty()
+    price: number
+
+    @IsString()
+    @ApiProperty()
+    address: string
+
+    @IsDate()
+    @ApiProperty()
+    dateStart: Date
+
+    @IsDate()
+    @ApiProperty()
+    dateEnd: Date
+
+    @IsString()
+    @ApiProperty()
+    tourPlaceFrom: string
+
+    @IsString()
+    @ApiProperty()
+    tourPlaceTo: string
+
+    @IsArray()
+    @ApiProperty()
+    images_Ids: string[]
+}
