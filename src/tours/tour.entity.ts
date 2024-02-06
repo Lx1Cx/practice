64ce8392e7533d730eb27d6a8f68413e7affa31c
lx1cx <lagunov.2019@list.ru>
2024-02-06 15:49:27 +0500

    Написана сущность туров

---
+++ src/tours/tour.entity.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger'
+
+export class TourEntity {
+    @ApiProperty()
+    id: string
+
+    @ApiProperty()
+    name: string
+
+    @ApiProperty()
+    description: string
+
+    @ApiProperty()
+    price: number
+
+    @ApiProperty()
+    address: string
+
+    @ApiProperty()
+    dateStart: Date
+
+    @ApiProperty()
+    dateEnd: Date
+
+    @ApiProperty()
+    tourPlaceFrom: string
+
+    @ApiProperty()
+    tourPlaceTo: string
+}
