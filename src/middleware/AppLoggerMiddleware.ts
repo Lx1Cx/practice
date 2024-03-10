7c417d4c526b40fb85048783bf3ee48bcbd09fe4
Lx1Cx <lagunov.2019@list.ru>
2024-03-10 16:40:41 +0500

    refactoring

--- src/middleware/AppLoggerMiddleware.ts
+++ src/middleware/AppLoggerMiddleware.ts
@@ -6,16 +6,17 @@ export class AppLoggerMiddleware implements NestMiddleware {
     private logger = new Logger('HTTP');
 
     use(request: Request, response: Response, next: NextFunction): void {
-        const { ip, method, path: url } = request;
+        const { ip, method, baseUrl } = request;
         const userAgent = request.get('user-agent') || '';
 
         response.on('close', () => {
             const { statusCode } = response;
-            const contentLength = response.get('content-length');
 
-            this.logger.log(
-                `${method} ${url} ${statusCode} ${contentLength} - ${userAgent} ${ip}`
-            );
+            if (baseUrl[1] !== "/") {
+                this.logger.log(
+                    `${method} ${baseUrl} ${statusCode} - ${userAgent} ${ip}`
+                );
+            }
         });
 
         next();
